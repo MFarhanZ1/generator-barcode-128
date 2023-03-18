d62a0744fc3d931a7bd604bce35cb5e249d69c4f
MFarhanZ1 <farhanaulia.p@gmail.com>
2023-03-18 13:01:17 +0700

    added feature in automatic convert being more flexible to choose file name and which value to convert to barcode in table

--- src/automatic_convert.py
+++ src/automatic_convert.py
@@ -7,7 +7,8 @@ def start_auto_convert():
     os.system("cls")
 
     get_dbs_name = input("[Input] Masukin Nama Databasenya : ")
-    get_table_name = input("\n[Input] Masukin Nama Tabelnya : ")    
+    get_table_name = input("\n[Input] Masukin Nama Tabelnya : ")  
+    get_col_name = input("\n[Input] Masukin nama kolom yang mau lu ubah jadi barcode : ")  
 
     # Mengecek apakah nama database dan nama tabelnya benar
     try:
@@ -24,14 +25,44 @@ def start_auto_convert():
         input("\nERROR 404 - Periksa kembali nama database/nama table anda! [teken 'Enter' buat mengisi ulang!]")
         start_auto_convert()
 
+    # mengambil semua nama kolom pada tabel terkait
+    colNameFetcherCursor = cnx.cursor()
+    query_fetcher_col_name = f"DESC {get_table_name}"
+    colNameFetcherCursor.execute(query_fetcher_col_name)
+    
+    # mengubah dalam bentuk list colomn yang diambil
+    column_names = [column[0] for column in colNameFetcherCursor.fetchall()]
+
+    # mencari indeks keberapa colomn terkait
+    while(True):
+        try:
+            indexKeyChoosen = column_names.index(get_col_name)
+            break
+        except ValueError:
+            get_col_name = input("\nHarap nama kolom sudah sesuai dengan yang ada di database!\n\n[Input] Silahkan input lagi Nama Kolomnya : ")
+            continue
+
+    get_custom_nama_file = input("\n[Optional] (Tekan 'Enter' jika ingin skip ini) Masukan kustom nama kolom yang ingin dijadikan nama file : ")
+
+    if get_custom_nama_file != "":
+        # mencari indeks keberapa colomn terkait
+        while(True):
+            try:            
+                indexKeyChoosenNamaFile = column_names.index(get_custom_nama_file)
+                break
+
+            except ValueError:
+                get_custom_nama_file = input("\nHarap nama kolom sudah sesuai dengan yang ada di database!\n\n[Input] Silahkan input lagi Nama Kolomnya : ")
+                continue
+
     print("\n[+] Proses sedang berjalan...")
 
     # Membuat cursor
     cursor = cnx.cursor()
 
-    # Melakukan query pada tabel 'nama_tabel'
-    query = f"SELECT * FROM {get_table_name}"
-    cursor.execute(query)
+    # Mengambil semua data pada tabel terkait
+    query_get_table_data = f"SELECT * FROM {get_table_name}"
+    cursor.execute(query_get_table_data)
 
     # Mengambil semua hasil query
     results = cursor.fetchall()
@@ -40,16 +71,21 @@ def start_auto_convert():
     for row in results:
 
         # Mengambil value kode_barang pada table mysql
-        text_kode_barang = row[0]
-        text_nama_barang = row[1]
-        
+        text_kode_barang = row[indexKeyChoosen]
+
+        if get_custom_nama_file != "":
+            text_nama_barang = row[indexKeyChoosenNamaFile]    
+            nama_file = f"{text_nama_barang} [{text_kode_barang}]"
+        else:
+            nama_file = text_kode_barang
+
         # Mengecek apakah kode barang berupa string atau tidak
         try:
             int(text_kode_barang)
             continue
         except ValueError:
             # Konversi ke code barcode 128
-            backend.text_to_barcode128.convert(text_kode_barang, image_file_name=f"{text_nama_barang} [{text_kode_barang}]", additional_path="barcode128_result/automate/")
+            backend.text_to_barcode128.convert(text_kode_barang, image_file_name=nama_file, additional_path="barcode128_result/automate/")
 
     # Menutup koneksi dan cursor
     cursor.close()
---
+++ teast.py
@@ -0,0 +1,4 @@
+get_custom_nama_file = input("\n[Optional] (Tekan 'Enter' jika ingin skip ini) Masukan kustom nama kolom yang ingin dijadikan nama file : ")
+
+if get_custom_nama_file == "":
+    print(311)
